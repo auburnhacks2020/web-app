c72e2e411ba494fe8326fe971c83a10a535c22f3
Bradley Probert <bradprobert96@gmail.com>
2019-10-07 16:37:06 -0500

    update dependencies

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
-  const [isFabActive, setFabActive] = useState(false)
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
@@ -21,7 +20,6 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
         <AppNavigator />
       </View>
     );
