22fae4bcbd1e2933a74267e1e9283196a50bc0fd
Bradley Probert <bradprobert96@gmail.com>
2019-10-27 16:57:04 -0500

    finished backend basic user auth

--- database/src/resolvers.js
+++ database/src/resolvers.js
@@ -1,45 +1,54 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const resolvers = {
-  Mutation: {
-    register: async (parent, { username, password }, ctx, info) => {
-      const hashedPassword = await bcrypt.hash(password, 10)
-      const user = await ctx.prisma.createUser({
-        username,
-        password: hashedPassword,
-      })
-      return user
-    },
-    login: async (parent, { username, password }, ctx, info) => {
-      const user = await ctx.prisma.user({ username })
-    
-      if (!user) {
-        throw new Error('Invalid Login')
-      }
-    
-      const passwordMatch = await bcrypt.compare(password, user.password)
-    
-      if (!passwordMatch) {
-        throw new Error('Invalid Login')
-      }
-    
-      const token = jwt.sign(
-        {
-          id: user.id,
-          username: user.email,
-        },
-        'my-secret-from-env-file-in-prod',
-        {
-          expiresIn: '30d', // token will expire in 30days
-        },
-      )
-      return {
-        token,
-        user,
-      }
-    }
-  }
-}
+	Query: {
+		currentUser: (parent, args, { user, prisma }) => {
+			// this if statement is our authentication check
+			if (!user) {
+				throw new Error('Not Authenticated');
+			}
+			return prisma.user({ id: user.id });
+		}
+	},
+	Mutation: {
+		register: async (parent, { username, password }, ctx, info) => {
+			const hashedPassword = await bcrypt.hash(password, 10);
+			const user = await ctx.prisma.createUser({
+				username,
+				password: hashedPassword
+			});
+			return user;
+		},
+		login: async (parent, { username, password }, ctx, info) => {
+			const user = await ctx.prisma.user({ username });
 
-module.exports = resolvers
+			if (!user) {
+				throw new Error('Invalid Login');
+			}
+
+			const passwordMatch = await bcrypt.compare(password, user.password);
+
+			if (!passwordMatch) {
+				throw new Error('Invalid Login');
+			}
+
+			const token = jwt.sign(
+				{
+					id: user.id,
+					username: user.email
+				},
+				process.env.DB_SECRET,
+				{
+					expiresIn: '30d' // token will expire in 30days
+				}
+			);
+			return {
+				token,
+				user
+			};
+		}
+	}
+};
+
+module.exports = resolvers;
