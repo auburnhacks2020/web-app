431d2214779c2dc1bc809328ea8e8ca1891885ba
Bradley Probert <bradprobert96@gmail.com>
2019-10-27 18:58:27 -0500

    added login form to profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Platform } from 'react-native';
-import { Header } from '../components';
+import React, {useState} from "react";
+import { ScrollView, View, StyleSheet, Platform } from "react-native";
+import { Header } from "../components";
+import { TextInput } from "react-native-paper";
 
 export default function ProfileScreen() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <ScrollView style={styles.container}>
       <Header title="Profile" />
+      <View style={styles.loginForm}>
+        <TextInput
+          label="Username"
+          value={username}
+          onChange={value => setUsername(value)}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          onChange={value => setPassword(value)}
+        />
+      </View>
     </ScrollView>
   );
 }
 
 ProfileScreen.navigationOptions = {
-  title: 'Profile',
+  title: "Profile"
 };
 
 const styles = StyleSheet.create({
   container: {
-		flex: 1,
-		paddingTop: Platform.OS === 'ios' ? 15 : 0,
-		backgroundColor: '#f7f7f7',
-	}
+    flex: 1,
+    paddingTop: Platform.OS === "ios" ? 15 : 0,
+    backgroundColor: "#f7f7f7"
+  },
+  loginForm: {
+    display: 'flex',
+
+  }
 });
