952d12a52281d70e489f37ba58ee7332d018ae2f
Bradley Probert <bradprobert96@gmail.com>
2019-11-03 11:45:12 -0600

    profile screen updates

--- constants/theme.js
+++ constants/theme.js
@@ -1,88 +1,88 @@
-import color from "color";
+import color from 'color';
 import { configureFonts } from 'react-native-paper';
-import colors from './Colors'
+import colors from './Colors';
 
 const fontConfig = {
-    default: {
-      regular: {
-        fontFamily: 'sans-serif',
-        fontWeight: 'normal',
-      },
-      medium: {
-        fontFamily: 'sans-serif-medium',
-        fontWeight: 'normal',
-      },
-      light: {
-        fontFamily: 'sans-serif-light',
-        fontWeight: 'normal',
-      },
-      thin: {
-        fontFamily: 'sans-serif-thin',
-        fontWeight: 'normal',
-      },
-    },
-  };
+	default: {
+		regular: {
+			fontFamily: 'sans-serif',
+			fontWeight: 'normal'
+		},
+		medium: {
+			fontFamily: 'sans-serif-medium',
+			fontWeight: 'normal'
+		},
+		light: {
+			fontFamily: 'sans-serif-light',
+			fontWeight: 'normal'
+		},
+		thin: {
+			fontFamily: 'sans-serif-thin',
+			fontWeight: 'normal'
+		}
+	}
+};
 
 const defaultTheme = {
-  dark: false,
-  roundness: 4,
-  colors: {
-    primary: "#6200ee",
-    accent: "#03dac4",
-    background: "#f6f6f6",
-    surface: colors.white,
-    error: "#B00020",
-    text: colors.black,
-    onBackground: "#000000",
-    onSurface: "#000000",
-    disabled: color(colors.black)
-      .alpha(0.26)
-      .rgb()
-      .string(),
-    placeholder: color(colors.black)
-      .alpha(0.54)
-      .rgb()
-      .string(),
-    backdrop: color(colors.black)
-      .alpha(0.5)
-      .rgb()
-      .string(),
-    notification: "pink"
-  },
-  fonts: configureFonts(fontConfig),
-  animation: {
-    scale: 1.0
-  }
+	dark: false,
+	roundness: 4,
+	colors: {
+		primary: '#DD550C',
+		accent: '#03dac4',
+		background: '#03244d',
+		surface: '#fff',
+		error: '#B00020',
+		text: '#fff',
+		onBackground: '#000000',
+		onSurface: '#000000',
+		disabled: color(colors.black)
+			.alpha(0.26)
+			.rgb()
+			.string(),
+		placeholder: color(colors.white)
+			.alpha(0.54)
+			.rgb()
+			.string(),
+		backdrop: color(colors.black)
+			.alpha(0.5)
+			.rgb()
+			.string(),
+		notification: 'pink'
+	},
+	fonts: configureFonts(fontConfig),
+	animation: {
+		scale: 1.0
+	}
 };
 
 const darkTheme = {
-  ...defaultTheme,
-  dark: true,
-  mode: "adaptive",
-  colors: {
-    ...defaultTheme.colors,
-    primary: "#BB86FC",
-    accent: "#03dac6",
-    background: "#121212",
-    surface: "#121212",
-    error: "#CF6679",
-    onBackground: "#FFFFFF",
-    onSurface: "#FFFFFF",
-    text: colors.white,
-    disabled: color(colors.white)
-      .alpha(0.38)
-      .rgb()
-      .string(),
-    placeholder: color(colors.white)
-      .alpha(0.54)
-      .rgb()
-      .string(),
-    backdrop: color(colors.black)
-      .alpha(0.5)
-      .rgb()
-      .string(),
-    notification: "lightpink"
-  }
+	...defaultTheme,
+	dark: true,
+	mode: 'adaptive',
+	colors: {
+		...defaultTheme.colors,
+		primary: '#BB86FC',
+		accent: '#03dac6',
+		background: '#121212',
+		surface: '#121212',
+		error: '#CF6679',
+		onBackground: '#FFFFFF',
+		onSurface: '#FFFFFF',
+		text: colors.white,
+		disabled: color(colors.white)
+			.alpha(0.38)
+			.rgb()
+			.string(),
+		placeholder: color(colors.white)
+			.alpha(0.54)
+			.rgb()
+			.string(),
+		backdrop: color(colors.black)
+			.alpha(0.5)
+			.rgb()
+			.string(),
+		notification: 'lightpink'
+	}
 };
 
 export { defaultTheme, darkTheme };
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,62 +1,83 @@
 import React, { useState } from 'react';
-import { ScrollView, View, StyleSheet, Platform } from 'react-native';
+import {
+	ScrollView,
+	View,
+	StyleSheet,
+	KeyboardAvoidingView
+} from 'react-native';
 import { Header } from '../components';
-import { TextInput, Surface, Button } from 'react-native-paper';
+import { TextInput, Surface, Button, withTheme } from 'react-native-paper';
 
-export default function ProfileScreen() {
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	loginForm: {
+		backgroundColor: 'transparent',
+		flex: 1,
+		margin: 10,
+		padding: 20,
+		maxWidth: 400
+	},
+	textInput: {
+		marginBottom: 10
+	},
+	formButtons: {
+		flex: 1,
+		flexDirection: 'row-reverse',
+		alignItems: 'flex-end',
+		paddingTop: 10
+	},
+	button: {
+		marginLeft: 10
+	}
+});
+
+const ProfileScreen = props => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
+	const { colors } = props.theme;
+
 	return (
-		<ScrollView style={styles.container}>
+		<ScrollView
+			style={StyleSheet.flatten([
+				styles.container,
+				{ backgroundColor: colors.background }
+			])}>
 			<Header title='Profile' />
-			<Surface style={styles.loginForm}>
-				<TextInput
-					mode='outlined'
-					label='Email'
-					value={email}
-					onChangeText={text => setEmail(text)}
-					style={styles.textInput}
-				/>
-				<TextInput
-					mode='outlined'
-					label='Password'
-					value={password}
-					onChangeText={text => setPassword(text)}
-					style={styles.textInput}
-				/>
-				<View style={styles.formButtons}>
-					<Button>Login</Button>
-					<Button>Sign Up</Button>
-				</View>
-			</Surface>
+			<KeyboardAvoidingView style={{ flex: 1 }}>
+				<Surface style={styles.loginForm}>
+					<TextInput
+						mode='outlined'
+						label='Email'
+						value={email}
+						onChangeText={text => setEmail(text)}
+						style={styles.textInput}
+					/>
+					<TextInput
+						mode='outlined'
+						label='Password'
+						value={password}
+						onChangeText={text => setPassword(text)}
+						style={styles.textInput}
+					/>
+					<View style={styles.formButtons}>
+						<Button style={styles.button} mode='contained'>
+							Login
+						</Button>
+						<Button style={styles.button} mode='contained'>
+							Sign Up
+						</Button>
+					</View>
+				</Surface>
+			</KeyboardAvoidingView>
 		</ScrollView>
 	);
-}
+};
 
 ProfileScreen.navigationOptions = {
 	title: 'Profile'
 };
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#03244d'
-	},
-	loginForm: {
-    flex: 1,
-		elevation: 4,
-		margin: 10,
-		padding: 20,
-    maxWidth: 400,
-	},
-	textInput: {
-    marginBottom: 10
-	},
-	formButtons: {
-		flex: 1,
-    flexDirection: 'row-reverse',
-    alignItems: 'flex-end',
-		paddingTop: 10
-	}
-});
+export default withTheme(ProfileScreen);
