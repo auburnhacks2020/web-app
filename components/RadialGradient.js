8b61ccef67a71176bfafab8cbd34640524392c1c
Bradley Probert <bradprobert96@gmail.com>
2019-11-12 10:34:05 -0600

    example radial gradient

--- components/RadialGradient.js
+++ components/RadialGradient.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import Svg from 'react-native-svg'
+import React from "react";
+import Svg from "react-native-svg";
+import { withTheme } from "react-native-paper";
 
 const RadialGradient = props => {
-    return (
-        <Svg styles={props.styles}>
+  const { colors } = props.theme;
 
-        </Svg>
-    )
-}
+  return (
+    <Svg styles={props.styles}>
+      <Defs>
+        <RadialGradient
+          id="grad"
+          cx="150"
+          cy="75"
+          rx="85"
+          ry="55"
+          fx="150"
+          fy="75"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop offset="0" stopColor="transparent" stopOpacity="1" />
+          <Stop offset="1" stopColor={colors.primary} stopOpacity="1" />
+        </RadialGradient>
+      </Defs>
+      <Ellipse cx="150" cy="75" rx="85" ry="55" fill="url(#grad)" />
+    </Svg>
+  );
+};
 
-export default RadialGradient
+export default withTheme(RadialGradient);
