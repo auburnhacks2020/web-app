073ee059bf103590307a0d65fc431c5a9bc32bed
Dillon Thompson <dj.thompson715@gmail.com>
2019-12-17 16:12:02 -0600

    finished profile page for now, added mlh banner also updated signout method.

--- constants/Stylesheet.js
+++ constants/Stylesheet.js
@@ -3,55 +3,58 @@ const text = "montserrat";
 const robotoMono = "roboto-mono";
 
 export default {
-  container: {
-    flex: 1,
-    backgroundColor: "#03244d"
-  },
-  row: {
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    width: "90%",
-    backgroundColor: "#000",
-    padding: 20,
-    alignSelf: "center",
-    borderRadius: 5,
-    marginBottom: 50
-  },
-  par: {
-    fontFamily: text,
-    fontSize: 17,
-    paddingTop: 10,
-    fontWeight: "400"
-  },
-  modalpar: {
-    fontWeight: "300",
-    fontSize: 17,
-    color: "#ffffff",
-  },
-  btn: {
-    backgroundColor: "#03244d"
-  },
-  cardbtn: {
-    backgroundColor: "#03244d",
-    alignSelf: "flex-end"
-  },
-  cardAction: {
-    flex: 1,
-    justifyContent: "flex-end",
-    padding: 10
-  },
-  cardContent: {
-    paddingTop: 10
-  },
-  links: {
-    color: "#f68026"
-  },
-  centerText: {
-    textAlign: 'center'
-  },
-  robotoMono,
-  heading,
-  text
+	container: {
+		flex: 1,
+		backgroundColor: '#03244d'
+	},
+	row: {
+		flex: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'space-between',
+		width: '90%',
+		backgroundColor: '#000',
+		padding: 20,
+		alignSelf: 'center',
+		borderRadius: 5,
+		marginBottom: 50
+	},
+	par: {
+		fontFamily: text,
+		fontSize: 17,
+		paddingTop: 10,
+		fontWeight: '400'
+	},
+	modalpar: {
+		fontWeight: '300',
+		fontSize: 17,
+		color: '#ffffff'
+	},
+	btn: {
+		backgroundColor: '#03244d'
+	},
+	btn2: {
+    backgroundColor: '#000',
+	},
+	cardbtn: {
+		backgroundColor: '#03244d',
+		alignSelf: 'flex-end'
+	},
+	cardAction: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		padding: 10
+	},
+	cardContent: {
+		paddingTop: 10
+	},
+	links: {
+		color: '#f68026'
+	},
+	centerText: {
+		textAlign: 'center'
+	},
+	robotoMono,
+	heading,
+	text
 };
