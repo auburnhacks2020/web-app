aee46f4e76b528e5fc87a4f0a8b6c9a6a5d0e635
Dillon Thompson <dj.thompson715@gmail.com>
2019-12-29 22:48:42 -0600

    fixed menu button partially. will finish tomorrow

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Platform, View, Text, StyleSheet, Image, SafeAreaView, TouchableOpacity } from 'react-native';
-import { Appbar } from 'react-native-paper';
 import { layout } from '../constants';
 import colors from '../constants/Colors';
 import fonts from '../constants/Fonts';
@@ -34,10 +33,22 @@ const styles = StyleSheet.create({
 });
 
 const AppHeader = (props) => {
+	const [signedIn, setSignedIn] = useState(null);
+	useEffect(() => {
+		if(signedIn === null) fetchSignInState();
+	}, [signedIn]);
+	const fetchSignInState = async () => {
+		try {
+			const status = await isSignedIn();
+			setSignedIn(status);
+		} catch (err) {
+			console.log(err);
+		}
+	};
     return (
 			<SafeAreaView style={styles.header}>
 				<View style={{alignSelf:'flex-start', position:'relative', top:33}}>
-				{isSignedIn === true ? 
+				{signedIn ?
 					<TouchableOpacity onPress={() => onSignOut().then(() => props.navigation.navigate('signIn'))}>
 						<Ionicons
 							name={Platform.OS == 'ios' ? 'ios-more' : 'md-more'}
@@ -73,5 +84,3 @@ const AppHeader = (props) => {
 }
 
 export default AppHeader
-
-{/* <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000" href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-white.svg" alt="Major League Hacking 2020 Hackathon Season" style="width:100%"></a> */}
