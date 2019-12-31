b2b12354f59ee60b7aff38d78af1d98677c8d72c
Dillon Thompson <dj.thompson715@gmail.com>
2019-12-31 14:58:57 -0600

    fixed mlh banner. app ready for deployment. need to add logout button afterwards.

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -7,31 +7,6 @@ import * as WebBrowser from 'expo-web-browser';
 import { isSignedIn, onSignOut } from '../auth';
 import { Ionicons } from '@expo/vector-icons';
 
-const styles = StyleSheet.create({
-	header: {
-        flex: 1,
-		flexDirection: 'column',
-		alignItems:'center',
-	},
-	container: {
-		flexDirection:'row',
-		position:'relative',
-		top: isSignedIn === true ? 83 : 98
-	},
-	auburn: {
-		color: colors.tintColor,
-		fontSize: 28,
-		fontFamily: fonts.heading,
-		alignSelf: 'center',
-	},
-	hacks: {
-		color: colors.white,
-		fontSize: 28,
-		fontFamily: fonts.robotoMono,
-		alignSelf: 'center'
-	}
-});
-
 const AppHeader = (props) => {
 	const [signedIn, setSignedIn] = useState(null);
 	useEffect(() => {
@@ -45,39 +20,37 @@ const AppHeader = (props) => {
 			console.log(err);
 		}
 	};
+
+	const styles = StyleSheet.create({
+		header: {
+			flex: 1,
+			flexDirection: 'column',
+			alignItems: 'center'
+		},
+		container: {
+			flexDirection: 'row',
+			position: 'relative',
+		},
+		auburn: {
+			color: colors.tintColor,
+			fontSize: 28,
+			fontFamily: fonts.heading,
+			alignSelf: 'center'
+		},
+		hacks: {
+			color: colors.white,
+			fontSize: 28,
+			fontFamily: fonts.robotoMono,
+			alignSelf: 'center'
+		}
+	});
+
+
     return (
 			<SafeAreaView style={styles.header}>
-				<View style={{alignSelf:'flex-start', position:'relative', top:33}}>
-				{signedIn ?
-					<TouchableOpacity onPress={() => onSignOut().then(() => props.navigation.navigate('signIn'))}>
-						<Ionicons
-							name={Platform.OS == 'ios' ? 'ios-more' : 'md-more'}
-							size={30}
-							color='#fff'
-							style={{ position: 'relative', top: 85, left: 25 }}
-						/>
-					</TouchableOpacity>
-				 : null}
-				</View>
 				<View style={styles.container}>
 					<Text style={styles.auburn}>AUBURN</Text>
 					<Text style={styles.hacks}>HACKS</Text>
-				</View>
-				<View style={{alignSelf:'flex-end', position:'relative', top: isSignedIn === true ? 33 : 45}}>
-					<TouchableOpacity
-						onPress={() =>
-							WebBrowser.openBrowserAsync(
-								'https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white'
-							)
-						}>
-						<Image
-							style={{
-								width: 100,
-								height: 200,
-							}}
-							source={require('../assets/logos/mlh-trust-badge-2020-white.png')}
-						/>
-					</TouchableOpacity>
 				</View>
 			</SafeAreaView>
 		);
