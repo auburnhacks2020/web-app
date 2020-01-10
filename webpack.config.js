d97a9e51992cf0dbe7352022fb2ea43e648e4da6
Dillon Thompson <dj.thompson715@gmail.com>
2020-01-10 16:06:31 -0600

    fixed bugs that alex reported

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
-  config.icon = './assets/logos/AuburnHacks-icon.png';
+  // config.icon = './assets/logos/AuburnHacks-icon.png';
   return config;
 };
