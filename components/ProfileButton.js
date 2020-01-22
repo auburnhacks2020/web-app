4982c10bc032e73d6fda96614e347a391413a19f
Bradley Probert <bradprobert96@gmail.com>
2020-01-22 14:23:42 -0600

    updated profile button

--- components/ProfileButton.js
+++ components/ProfileButton.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Platform, StyleSheet, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import ActiveTabGradient from './ActiveTabGradient';
 import { Button, withTheme, Menu, Provider } from 'react-native-paper';
 import { Colors } from '../constants';
+import { isSignedIn, onSignOut } from '../auth';
 
 const SIZE = Platform.OS === 'web' ? 80 : 70;
 
@@ -12,6 +13,9 @@ const styles = StyleSheet.create({
 		flex: 1,
 		flexDirection: 'row',
 		justifyContent: 'center'
+	},
+	menu: {
+		
 	},
 	button: {
 		top: -SIZE / 2,
@@ -28,37 +32,77 @@ const ProfileButton = props => {
 	const { routeName, onPress, theme } = props;
 	const { colors } = theme;
 	const [isVisible, setVisible] = useState(false);
+	const [signedIn, setSignedIn] = useState(null);
+
+	useEffect(() => {
+		if (signedIn === null) fetchSignInState();
+	}, [signedIn]);
+
+	const fetchSignInState = async () => {
+		try {
+			const status = await isSignedIn();
+			setSignedIn(status);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	const handlePress = () => {
-		onPress();
 		setVisible(!isVisible);
 	};
 
 	return (
 		<View style={styles.container}>
-			<Provider theme={theme}/>
-			<Button
-				theme={theme}
-				mode='outlined'
-				onPress={handlePress}
-				style={styles.button}>
-				<Ionicons
-					name='md-person'
-					size={SIZE / 1.5}
-					color={
-						routeName === 'ProfileStack'
-							? Colors.iconSelected
-							: Colors.iconDefault
-					}
-				/>
-			</Button>
 			<Menu
+				style={styles.menu}
 				visible={isVisible}
 				onDismiss={() => setVisible(false)}
-				anchor={{}}>
-				<Menu.Item onPress={() => {}} title='Item 1' />
-				<Menu.Item onPress={() => {}} title='Item 2' />
-				<Menu.Item onPress={() => {}} title='Item 3' />
+				anchor={
+					<Button
+						theme={theme}
+						mode='outlined'
+						onPress={handlePress}
+						style={styles.button}>
+						<Ionicons
+							name='md-person'
+							size={SIZE / 1.5}
+							color={
+								routeName === 'ProfileStack'
+									? Colors.iconSelected
+									: Colors.iconDefault
+							}
+						/>
+					</Button>
+				}>
+				{signedIn ? (
+					<View>
+						<Menu.Item
+							onPress={() => {
+								props.navigation.navigate('home');
+								setVisible(false);
+							}}
+							title='Profile'
+						/>
+						<Menu.Item
+							onPress={() => {
+								onSignOut();
+								props.navigation.navigate('ProfileStack');
+								setVisible(false);
+							}}
+							title='Logout'
+						/>
+					</View>
+				) : (
+					<View>
+						<Menu.Item
+							onPress={() => {
+								props.navigation.navigate('ProfileStack');
+								setVisible(false);
+							}}
+							title='Login'
+						/>
+					</View>
+				)}
 			</Menu>
 		</View>
 	);
